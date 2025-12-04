8abea6bbf423fda8a3e2e672c79926676421d5a8
MuTuOz <atropeyzaj@hotmail.com>
2025-12-04 23:28:17 +0300

    Database Connection added

--- hms/src/main/java/module-info.java
+++ hms/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.hospitalmanagement {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens com.hospitalmanagement to javafx.fxml;
     exports com.hospitalmanagement;
