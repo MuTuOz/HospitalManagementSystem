381780a4c151c132f9d1e45609a54c66b819fe35
bozkurt1453-am <141610546+bozkurt1453-am@users.noreply.github.com>
2025-12-04 23:45:55 +0300

    DoctorDashboardController and LoginController are added

---
+++ hms/src/main/java/com/hospitalmanagement/DoctorDashboardController.java
@@ -0,0 +1,26 @@
+package com.cs320;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class DoctorDashboardController {
+
+    @FXML
+    private Label welcomeLabel;
+
+    @FXML
+    private void initialize() {
+        welcomeLabel.setText("Doktor Dashboard'a Hoş Geldiniz!");
+    }
+
+    @FXML
+    private void handleLogout() {
+
+        try {
+            System.out.println("Doktor çıkış yaptı.");
+            App.setRoot("login");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
