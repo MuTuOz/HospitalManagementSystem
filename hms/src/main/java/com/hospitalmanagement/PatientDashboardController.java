702170535e5c53976da119791b11ee1afea779d0
MuTuOz <atropeyzaj@hotmail.com>
2025-12-07 22:42:40 +0300

    Uploaded Patient Dashboard

---
+++ hms/src/main/java/com/hospitalmanagement/PatientDashboardController.java
@@ -0,0 +1,24 @@
+package com.hospitalmanagement;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class PatientDashboardController {
+    @FXML
+    private Label welcomeLabel;
+
+    @FXML
+    private void initialize() {
+        welcomeLabel.setText("Hasta Dashboard'a Hoş Geldiniz!");
+    }
+
+    @FXML
+    private void handleLogout() {
+
+        try {
+            App.setRoot("login");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
