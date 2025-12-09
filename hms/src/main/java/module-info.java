95035ef05da6987f23d0a5932bd69c56ec94d85f
ozgur-yilmazz <ylmazozgur03@gmail.com>
2025-12-09 23:29:05 +0300

    fix: update module-info.java for new dashboard controllers

--- hms/src/main/java/module-info.java
+++ hms/src/main/java/module-info.java
@@ -2,7 +2,9 @@ module com.hospitalmanagement {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires javafx.graphics;
 
     opens com.hospitalmanagement to javafx.fxml;
+
     exports com.hospitalmanagement;
 }
